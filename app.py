7da5317f856db97bf4a3c6a1cea95e8cde700da2
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2025-10-21 09:29:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,10 @@ import streamlit as st
 
 st.header("mon application")
 
-st.write("Hamilton")
+nombres = [1, 2, 4, 7]
+carre = [1**2, 2**2, 4**2, 7**2]
+
+d = {"nombres": nombres, "carre": carre}
+data = pd.DataFrame(d)
+
+st.dataframe(data)
